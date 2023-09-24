ffe1b42b664cf9ee7aeb73c1aeff1cacd05cf51e
Emiliano1919 <emilianojg38@gmail.com>
2023-09-24 09:41:51 -0400

    Add contact information

--- src/tab/contact.js
+++ src/tab/contact.js
@@ -4,15 +4,27 @@ function contactPage(){
     const infoContact = document.createElement('div');
     infoContact.classList.add('infoContact');
     infoContact.classList.add('hidden');
-    const title = document.createElement('title');
+    const title = document.createElement('h1');
     title.classList.add('title');
     title.textContent='Contacto';
     const information = document.createElement('div');
     information.classList.add('information');
+    const name = document.createElement('h2');
+    name.textContent='Chef Martin';
+    const list = document.createElement('ul');
+    const telephone = document.createElement('li');
+    telephone.textContent='+52 XXXX XXXX';
+    const email = document.createElement('li');
+    email.textContent ='randomEmail@gmail.com';
+
 
     main.appendChild(infoContact);
     infoContact.appendChild(title);
     infoContact.appendChild(information);
+    information.appendChild(name);
+    information.appendChild(list);
+    list.appendChild(telephone);
+    list.appendChild(email);
 };
 
 export {contactPage};
